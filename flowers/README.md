b8fe17e61140b8327a269197d382ed45303ce5e5
Mfarzana <mfarzana1133@gmail.com>
2021-01-31 02:06:13 +0900

    Flowers Fact Project

---
+++ flowers/README.md
@@ -0,0 +1,2 @@
+# Flowers 
+This project shows different flowers and clicks each flower to show details about the flowers
---
+++ flowers/src/flowers.js
@@ -0,0 +1,14 @@
+export const flowers={
+    sunflower:{
+        image:'https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Sunflower_sky_backdrop.jpg/220px-Sunflower_sky_backdrop.jpg',
+        fact:["Sunflowers (Helianthus annuus) is an annual plant with a large daisy-like flower face. Its scientific name comes from the Greek words helios (“sun”) and anthos (“flower”)."]
+    },
+    tulip:{
+        image:'https://www.gardeningknowhow.com/wp-content/uploads/2008/07/tulips.jpg',
+        fact:["The tulip, Tulipa spp., is a colorful perennial flower in the Liliaceae, or lily family. It’s best suited to growing in USDA Hardiness Zones 3 to 7."]
+    },
+    rose:{
+        image:'https://s3.eu-west-2.amazonaws.com/jparkers/1000/0001691.jpg',
+        fact:["A rose is a woody perennial flowering plant of the genus Rosa, in the family Rosaceae, or the flower it bears.[1] There are over three hundred species and tens of thousands of cultivars"]
+    }
+}
